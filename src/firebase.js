5313c6f00e9359f178e687017ccefa4a730d5a8c
sriyncollege <shriyncollege@gmail.com>
2025-12-28 18:48:58 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCHjAm0Zw9MhdTd5DhquaLdzYHb5tmmmkQ",
   authDomain: "education-nexus-of-india.firebaseapp.com",
   projectId: "education-nexus-of-india",
-  storageBucket: "education-nexus-of-india.appspot.com",
+  storageBucket: "education-nexus-of-india.firebasestorage.app",
   messagingSenderId: "1079227729828",
   appId: "1:1079227729828:web:8aefd7ffacfad2326ae331"
 };
@@ -14,8 +14,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-
-// 🔴 THIS LINE IS CRITICAL
-setPersistence(auth, browserLocalPersistence);
-
 export const db = getFirestore(app);
